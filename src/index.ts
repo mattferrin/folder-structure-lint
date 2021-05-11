a36322ffb5097538fe238bb79138df0fccae6181
Matt Ferrin <mattferrin@gmail.com>
2021-05-11 08:05:03 -0700

    fix exit with failure to fail husky

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { execute } from "./functions/execute";
 execute().then((executed): number => {
   switch (executed.tag) {
     case "exit-and-fail":
-      throw new Error(executed.message);
+      console.error(executed.message);
+      process.exit(1);
     case "exit-and-pass":
       console.info("No rule violations detected");
       return 0;
